d95024711cc52b1093c95f11f4ec995491565d5a
JamieDesktop <roederjamie@gmail.com>
2017-02-23 21:12:30 -0600

    Fixed issue with finding the correct file path for images

--- Scripts/HomeScripts.js
+++ Scripts/HomeScripts.js
@@ -1,6 +1,6 @@
 ﻿
 function resetBackground() {
-    document.body.style.backgroundImage = "url('../Images/Onepunch.jpg')";
+    document.body.style.backgroundImage = "url('Images/Onepunch.jpg')";
     document.body.style.backgroundPosition = "top left";
     document.body.style.backgroundSize = "100% auto";
 }
@@ -12,30 +12,32 @@ function switchBackground(bottomImg) {
 
     switch (bottomImg.id) {
         case "tatsumakiPic":
-            document.body.style.backgroundImage = "url('../Images/TatsumakiBackground.jpg')";
+            document.body.style.backgroundImage = "url('Images/TatsumakiBackground.jpg')";
             document.body.style.backgroundPosition = "top left";
             document.body.style.backgroundSize = "100% auto";
+            fadeBackground();
             break;
         case "genosPic":
-            document.body.style.backgroundImage = "url('../Images/GenosBackground.jpg')";
+            document.body.style.backgroundImage = "url('Images/GenosBackground.jpg')";
             document.body.style.backgroundPosition = "top left";
             document.body.style.backgroundSize = "100% auto";
             break;
         case "saitamaPic":
-            document.body.style.backgroundImage = "url('../Images/SaitamaBackground.png')";
+            document.body.style.backgroundImage = "url('Images/SaitamaBackground.png')";
             document.body.style.backgroundPosition = "top left";
             document.body.style.backgroundSize = "100% auto";
             break;
         case "riderPic":
-            document.body.style.backgroundImage = "url('../Images/RiderBackground.jpg')";
+            document.body.style.backgroundImage = "url('Images/RiderBackground.jpg')";
             document.body.style.backgroundPosition = "top left";
             document.body.style.backgroundSize = "100% auto";
             break;
         case "sonicPic":
-            document.body.style.backgroundImage = "url('../Images/SonicBackground.png')";
+            document.body.style.backgroundImage = "url('Images/SonicBackground.png')";
             document.body.style.backgroundPosition = "top left";
             document.body.style.backgroundSize = "100% auto";
             break;
     }
-
+    }
 }
+
